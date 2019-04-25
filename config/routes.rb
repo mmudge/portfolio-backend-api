dcd0a2e392b21a29a56da1e713820697e2335531
michael mudge <michaeltmudge@gmail.com>
2019-04-25 08:35:21 +0200

    initial commit, configured api only back end with devise and devise-jwt gems

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
+
+  resources :messages
+  root to: 'messages#index'
+end
