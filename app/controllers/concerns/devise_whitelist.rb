8f50f6d14e60c63b2e95643c1ae2f50891a96376
michael mudge <michaeltmudge@gmail.com>
2019-05-07 19:53:45 +0200

    added username to devise whitelist params

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -6,7 +6,7 @@ module DeviseWhitelist
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_pic])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile_pic])
   end
 end
