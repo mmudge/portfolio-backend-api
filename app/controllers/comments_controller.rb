498f185577e17476f311c97590c7270281b70774
michael mudge <michaeltmudge@gmail.com>
2019-07-31 10:39:59 +0200

    working on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,9 +16,9 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     # comment = Comment.new(comment_params)
-    post.comments.build(comment_params)
-    
-    if post.comments.save!
+    comment = post.comments.build(comment_params)
+
+    if comment.save!
       render json: comment, status: :created
     else
       render json: comment.errors, status: :unprocessable_entity
@@ -45,6 +45,6 @@ class CommentsController < ApplicationController
 
 
     def comment_params
-      params.require(:comment).permit(:remark, :user_id)
+      params.require(:comment).permit(:remark, :user_id, :post_id)
     end
 end
