352b7db111dcbab5e4803834d23c1f89b7a5ff02
michael mudge <michaeltmudge@gmail.com>
2019-08-03 08:42:33 +0200

    adding as_json methods overrides

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :object, polymorphic: true
 
-
-
   def author
     user
+  end
+
+   def as_json(options={})
+    super.merge(user: user)
   end
 end
