e7cc44f754d240939f0c3364b0733ed93644afef
michael mudge <michaeltmudge@gmail.com>
2020-12-24 15:00:26 -0800

    updated cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:8080', /mmudge/]
+    origins ['http://localhost:8080', 'http://www.michaelmudge.com', 'https://www.michaelmudge.com']
 
     resource '*',
       headers: :any,
