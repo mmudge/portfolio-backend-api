6e893913ecc1325ed63e48db8423388848d6b6d0
michael mudge <michaeltmudge@gmail.com>
2020-12-26 14:28:30 -0800

    trying to set devise jwt env var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,13 +301,17 @@ Devise.setup do |config|
   config.jwt do |jwt|
     # jwt.secret = ENV['DEVISE_SECRET_KEY']
     # jwt.secret = Rails.application.credentials.secret_key_base
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key
+    # jwt.secret = Rails.application.credentials.devise_jwt_secret_key
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+
     jwt.dispatch_requests = [
-        ['POST', %r{^/login$}, %r{^/sign_in$}]
+        ['POST', %r{^/login$}]
       ]
-      jwt.revocation_requests = [
+
+    jwt.revocation_requests = [
         ['DELETE', %r{^/logout$}]
       ]
+
     jwt.expiration_time = 1.days.to_i
   end
 end
